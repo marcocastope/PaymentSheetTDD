0536819381c258205b45bb9c013046d02a6ad7c7
Marco Castope <marcocastope7@gmail.com>
2024-02-22 14:10:06 -0500

    first release

---
+++ PaymentSheet/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PaymentSheet
+//
+//  Created by Marco Castope on 22/02/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PaymentSheet/PaymentSheetApp.swift
@@ -0,0 +1,17 @@
+//
+//  PaymentSheetApp.swift
+//  PaymentSheet
+//
+//  Created by Marco Castope on 22/02/24.
+//
+
+import SwiftUI
+
+@main
+struct PaymentSheetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
